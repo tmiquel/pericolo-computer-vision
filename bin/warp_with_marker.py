848833e4e48f18d9367aedb88f3af17d68227d7a
wbenbihi <waligoo@gmail.com>
2020-01-12 04:22:53 +0100

    [ADD] bin scripts

---
+++ bin/warp_with_marker.py
@@ -0,0 +1,11 @@
+from libs.image_warper import ImageWarper
+from skimage import io
+
+if __name__ == '__main__':
+    import sys
+    image_name = sys.argv[-1]
+    save_name = '.'.join(image_name.split('.')[:-1]) + '_warped.png'
+    iw = ImageWarper(image_name)
+    print("Rectifying {}".format(image_name))
+    result_image = iw.warp_with_marker()
+    io.imsave(save_name, result_image)
